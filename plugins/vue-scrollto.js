3dcb72da0ddcf32a5ebe022491ca1032970cb4cb
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-10-13 09:33:04 +0900

    ページ内スクロールの作成

---
+++ plugins/vue-scrollto.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import VueScrollTo from 'vue-scrollto'
+
+Vue.use(VueScrollTo, {
+  /* オプション：https://www.npmjs.com/package/vue-scrollto#options */
+  duration: 1000,
+  easing: "ease"
+})
