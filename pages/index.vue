05d4cd868f47c35e2f5744c7f97da366cc068031
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-12-19 22:27:47 +0900

    youtubeページの作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,63 @@
-<template lang="pug">
-div
-  p pタグです
-  p pタグです#[span.span spanタグです]pタグの続きです
-
-  ul
-    li: a aタグ1
-    li: a aタグ2
-    li: a aタグ3
-
-  #id IDを設定
-  .class CLASSを設定
-
-  // これはHTMLに表示されるコメントです
-  //- これはHTMLに表示されないコメントです
-
-  ul
-    li(
-    v-for="i in items",
-    :key="i") {{i}}
-
-  button(@click="click") Click Me!
+<template>
+  <div>
+    <youtube
+      v-if="show"
+      :video-id="videoId"
+      :player-vars="playerVars"
+      :width="width"
+      :height="height"
+      @playing="playing"
+      @ended="ended"
+    />
+    <!-- <iframe ref="test" width="560" height="315" :src="`https://www.youtube.com/embed/${videoId}?enablejsapi=1`" frameborder="0" allowfullscreen></iframe>
+    <button @click="test">test</button> -->
+  </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import VueYoutube from 'vue-youtube'
+
+Vue.use(VueYoutube)
+
 export default {
   data() {
     return {
-      items: [
-        1,
-        2,
-        3
-      ]
+      videoId: 'lG0Ys-2d4MA',
+      // 参考：https://developers.google.com/youtube/player_parameters.html?playerVersion=HTML5#Parameters
+      playerVars: {
+        // autoplay: 1,
+        // controls: 0,
+        // loop: 1,
+        // start: 10,
+        // end: 15,
+        enablejsapi: 1,
+        origin: 'http://localhost:3000',
+      },
+      width: 400,
+      height: 200,
+      show: false,
     }
   },
   methods: {
-    click() {
-      console.log('click');
+    // 参考：https://www.npmjs.com/package/vue-youtube#events
+    playing() {
+      console.log('開始');
+    },
+    ended() {
+      console.log('終了');
+    },
+    test() {
+      // 参考：https://qiita.com/konweb/items/ab414d75ecddfb0cf4e9
+      // console.log('test');
+      this.$refs.test.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
     }
+  },
+  created() {
+    // console.log('ページ')
+    setTimeout(() => {
+      this.show = true;
+    }, 5000);
   }
 }
 </script>
